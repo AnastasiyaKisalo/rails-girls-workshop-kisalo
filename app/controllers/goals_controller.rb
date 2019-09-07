fc3820b6d9b8afad598a2cef26420794b6734e36
AnastasiyaKisalo <anastasiyakisalo@gmail.com>
2019-09-07 14:03:27 +0300

    Create app 'Be Focused'

---
+++ app/controllers/goals_controller.rb
@@ -0,0 +1,35 @@
+class GoalsController < ApplicationController
+	def index
+	  @goals = filtered_goals
+  	  @goals.order('complete, due_date, priority DESC, title')
+	end
+
+	def new
+	  @goal = Goal.new
+	end
+
+	def create
+	  @goal = Goal.new(goal_params)
+	  if @goal.save
+	    flash[:success] = 'Your goal has been successfully created'
+	    redirect_to goals_path
+	  else
+	    flash.now[:error] = 'Please fill in all necessary fields.'
+	    render 'new'
+	  end
+	end
+
+	def goal_params
+	  params.require(:goal).permit(:title, :description, :priority, :due_date, :complete)
+	end
+
+	def filtered_goals
+	  if params[:filter] == 'completed'
+	    Goal.completed
+	  elsif params[:filter] == 'incomplete'
+	    Goal.incomplete
+	  else
+	    Goal.all
+	  end
+	end
+end
